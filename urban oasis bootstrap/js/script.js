cbaa8f4bf5670b37c77fd27deead6591e0e8945a
andreimotian <31732261+andreimotian@users.noreply.github.com>
2021-06-15 14:33:38 +0200

    Add files via upload
    bg color van body, nav en footer veranderen los van elkaar met js

--- urban oasis bootstrap/js/script.js
+++ urban oasis bootstrap/js/script.js
@@ -1,7 +1,20 @@
 //variabel met array waar de kleuren in zitten
-var colors = ['#008CEE', '#359C55', '#FEC305'];
+var colorsBody = ['#008CEE', '#359C55', '#FEC305'];
+var colorsNav = ['#FA3E92', '#5CCB91', '#9F67CE'];
+var colorsFooter = ['#008CEE', '#359C55', '#FEC305'];
 
-//functie om de kleur te laten veranderen
+//functies om de kleur te laten veranderen bij het refreshen
 var changeBackground = function () {
-    document.body.style.background = colors[Math.floor(Math.random()*colors.length)];
+    document.querySelector('body').style.background = colorsBody[Math.floor(Math.random() * colorsBody.length)];
 }
+
+var changeNavBg = function () {
+    document.querySelector('nav').style.background = colorsNav[Math.floor(Math.random() * colorsNav.length)];
+}
+
+var changeFooterBg = function () {
+    document.querySelector('footer', 'footer section').style.background = colorsFooter[Math.floor(Math.random() * colorsFooter.length)];
+}
+
+//Als de pagina laad worden deze functies uitgevoerd
+window.addEventListener("load", changeBackground(), changeNavBg(), changeFooterBg());
