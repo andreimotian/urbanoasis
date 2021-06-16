902543aee691ccb9e4e86775b1e52ca913ec2d66
andreimotian <31732261+andreimotian@users.noreply.github.com>
2021-06-16 16:17:30 +0200

    Add files via upload

--- urban oasis bootstrap/js/script.js
+++ urban oasis bootstrap/js/script.js
@@ -2,6 +2,7 @@
 var colorsBody = ['#008CEE', '#359C55', '#FEC305'];
 var colorsNav = ['#FA3E92', '#5CCB91', '#9F67CE'];
 var colorsFooter = ['#008CEE', '#359C55', '#FEC305'];
+var colorsTekst = ['#000000', '#FFFFFF'];
 
 //functies om de kleur te laten veranderen bij het refreshen
 var changeBackground = function () {
@@ -16,5 +17,9 @@ var changeFooterBg = function () {
     document.querySelector('footer', 'footer section').style.background = colorsFooter[Math.floor(Math.random() * colorsFooter.length)];
 }
 
+var changeTekst = function () {
+    document.querySelector('h1', 'h2', 'p', 'main p').style.color = colorsTekst[Math.floor(Math.random() * colorsTekst.length)];
+}
+
 //Als de pagina laad worden deze functies uitgevoerd
-window.addEventListener("load", changeBackground(), changeNavBg(), changeFooterBg());
+window.addEventListener("load", changeBackground(), changeNavBg(), changeFooterBg(), changeTekst());
