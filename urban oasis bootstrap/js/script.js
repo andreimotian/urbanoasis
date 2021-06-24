c71eb047480234e603ed6f2086e5cb13530998ef
andreimotian <31732261+andreimotian@users.noreply.github.com>
2021-06-24 16:51:28 +0200

    Add files via upload

--- urban oasis bootstrap/js/script.js
+++ urban oasis bootstrap/js/script.js
@@ -1,26 +1,69 @@
+var palletRoodRoze = ['#FFDDFD', '#E56193', '#EE3C4A'];
+var palletGeelGroenBlauw = ['#359C55', '#018CEB', '#FFC300'];
+var palletRoodGeelGroen = ['#ED503F', '#41AC80', '#F2D452'];
 
-
-//variabel met array waar de kleuren in zitten
-var colorsBody = ['#FFDDFD', '#E56193', '#EE3C4A'];
-var colorsNav = ['#FFDDFD', '#E56193', '#EE3C4A'];
-var colorsFooter = ['#FFDDFD', '#E56193', '#EE3C4A'];
-//Kleurcombinaties
-//['#FFDDFD', '#E56193', '#EE3C4A'] Rood en roze
-//['#359C55', '#018CEB', '#FFC300'] Geel groen en blauw
-//['#ED503F', '#41AC80', '#F2D452'] Rood geel en groen
-
-//functies om de kleuren te veranderen bij het laden of refreshen van de website
-var changeBackground = function () {
-    document.querySelector('body').style.background = colorsBody[Math.floor(Math.random() * colorsBody.length)];
+//set 1
+var palletEenKleurEen = function () {
+    document.querySelector('body').style.background = palletRoodRoze[0];
 };
 
-var changeNavBg = function () {
-    document.querySelector('nav').style.background = colorsNav[Math.floor(Math.random() * colorsNav.length)];
+var palletEenKleurTwee = function () {
+    document.querySelector('nav').style.background = palletRoodRoze[1];
 };
 
-var changeFooterBg = function () {
-    document.querySelector('footer', 'footer section').style.background = colorsFooter[Math.floor(Math.random() * colorsFooter.length)];
+var palletEenKleurDrie = function () {
+    document.querySelector('footer, footer section').style.background = palletRoodRoze[2];
 };
 
-//Als de pagina laad worden deze functies uitgevoerd
-window.addEventListener("load", changeBackground(), changeNavBg(), changeFooterBg(), changeTekst());
+//set 2
+var palletTweeKleurEen = function () {
+    document.querySelector('body').style.background = palletGeelGroenBlauw[0];
+};
+
+var palletTweeKleurTwee = function () {
+    document.querySelector('nav').style.background = palletGeelGroenBlauw[1];
+};
+
+var palletTweeKleurDrie = function () {
+    document.querySelector('footer, footer section').style.background = palletGeelGroenBlauw[2];
+};
+
+//set 3
+var palletDrieKleurEen = function () {
+    document.querySelector('body').style.background = palletRoodGeelGroen[0];
+};
+
+var palletDrieKleurTwee = function () {
+    document.querySelector('nav').style.background = palletRoodGeelGroen[1];
+};
+
+var palletDrieKleurDrie = function () {
+    document.querySelector('footer, footer section').style.background = palletRoodGeelGroen[2];
+};
+
+//palletsets in een functie per pallet
+var palletEen = function () {
+    palletEenKleurEen();
+    palletEenKleurTwee();
+    palletEenKleurDrie();
+};
+
+var palletTwee = function () {
+    palletTweeKleurEen();
+    palletTweeKleurTwee();
+    palletTweeKleurDrie();
+};
+
+var palletDrie = function () {
+    palletDrieKleurEen();
+    palletDrieKleurTwee();
+    palletDrieKleurDrie();
+};
+
+var kleurPalletten = [palletEen, palletTwee, palletDrie];
+
+var laadPallet = function () {
+    kleurPalletten[Math.floor(Math.random() * kleurPalletten.length)]();
+};
+
+window.addEventListener("load", laadPallet());
